36aee61c1cd8cd8975804f9fbb4a04a55a9659b6
orenstarfishapp <oren.starfishapp@gmail.com>
2024-09-28 12:38:11 -0700

    Modal  Changes

--- src/pages/NavPages/HomePage.tsx
+++ src/pages/NavPages/HomePage.tsx
@@ -4,8 +4,7 @@ import Hero from "../../components/HomePage/Hero";
 import ContactForm from "../../components/HomePage/ContactForm";
 import ServiceCards from "../../components/HomePage/ServiceCards/ServiceCards";
 import FeatureCards from "../../components/HomePage/ServiceCards/FeatureCards/FeatureCards";
-//import ContactSection from "../../components/HomePage/ContactSection/ContactSection";
-import ServiceShowcase from "../../components/HomePage/ServiceShowcase";
+import ContactSection from "../../components/HomePage/ContactSection/ContactSection";
 import ServiceTitle from "../../components/HomePage/ServiceTitle";
 import Services from "../../components/HomePage/ContactForm/Services";
 // import HebrewTyping from "../../components/HebrewTyping";
@@ -19,7 +18,7 @@ const HomePage: React.FC = () => {
       <ServiceTitle />
       <ServiceCards />
       <FeatureCards />
-      <ServiceShowcase /> 
+      <ContactSection />
       <ContactForm />
       <div className="flex flex-nowrap mx-auto w-full px-[10px]">
         <div className=" flex py-[10px] min-w-full"></div>
