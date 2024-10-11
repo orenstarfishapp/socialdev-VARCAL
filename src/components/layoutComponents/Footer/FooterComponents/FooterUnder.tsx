039c6f55b0e661a14978d771074ec8f00c73bb12
poppydev-web <pinkypoppy0806@outlook.com>
2024-10-11 21:19:28 +0700

    Added footer image

--- src/components/layoutComponents/Footer/FooterComponents/FooterUnder.tsx
+++ src/components/layoutComponents/Footer/FooterComponents/FooterUnder.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import footerImage from "../../../../assets/images/image_2024_10_11T14_09_58_211Z.png";
 
 const FooterUnder: React.FC = () => {
   return (
-    <footer className="flex flex-col justify-center items-center  pt-px w-full text-sm leading-5 text-center text-white border-t border-solid bg-[#35023D] border-t-slate-400 border-t-opacity-20  max-md:px-5 max-md:max-w-full">
+    <footer className="flex flex-col justify-center items-center pt-px w-full text-sm leading-5 text-center text-white border-t border-solid bg-[#35023D] border-t-slate-400 border-t-opacity-20 max-md:px-5 max-md:max-w-full">
       <div className="flex flex-col flex-1 justify-center mx-auto py-[100px] max-w-[1140px] w-full">
         <div className="flex flex-col flex-1 w-full max-md:max-w-full">
           <div className="w-full max-md:px-5 max-md:max-w-full">
-            © 2023 AI Web Dev | כל הזכויות שמורות
+            <img 
+              src={footerImage}
+              alt="Footer Logo" 
+              className="mb-4"
+            />
+            <div>© 2023 AI Web Dev | כל הזכויות שמורות</div>
           </div>
         </div>
       </div>
